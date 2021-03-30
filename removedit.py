1f9e4030665082e193bee9a7813cc960b2e239ec
reuben <reusharino@gmail.com>
2021-03-30 13:36:30 +1300

    Update removedit.py

--- removedit.py
+++ removedit.py
@@ -3,12 +3,9 @@ import subprocess,os,sys,random,time,urllib3,subprocess,base64
 def run(cmd):                                                  
      subprocess.call(cmd, shell=True)
 
-run("mkdir edit")
-run("mkdir edit/bot")
-run("mkdir edit/cnc")
-
 run("mv edit/bot/table.c JG/mirai/bot")
 
-run("mv edit/cnc/* JG/mirai/cnc")
+run("mv edit/cnc/admin.go JG/mirai/cnc")
+run("mv edit/cnc/main.go JG/mirai/cnc")
 
 run("rm -rf edit")
